44f4a445061f8ec778148f7069902a07f2e91d50
Vrushabh <vrushabhgawas14@gmail.com>
2025-01-18 19:40:17 +0530

    login registration error message

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,9 +2,11 @@ import LoginRegistrationForm from "../components/LoginRegForm";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 import { loginUserWithEmailAndPassword } from "../firebase/auth";
+import { useState } from "react";
 
 export default function Login() {
   const { userLoggedIn } = useAuth();
+  const [errorMessage, setErrorMessage] = useState("");
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -12,18 +14,41 @@ export default function Login() {
       const formData = new FormData(e.currentTarget);
       const email = await formData.get("userEmail")?.toString().toLowerCase();
       const password = await formData.get("userPassword")?.toString();
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (!email?.match(emailRegex)) {
+        setErrorMessage("Please enter a valid email address.");
+        return;
+      }
+
+      if (password?.length! < 6) {
+        setErrorMessage("Password must be at least 6 characters long.");
+        return;
+      }
 
       await loginUserWithEmailAndPassword(email!, password!);
 
-      // alert(`User Login Successfully! ${userCredentials.user.email}`);
-    } catch (error) {
-      alert(`Error in user login: ${error}`);
+      setErrorMessage("User Login Successfully!");
+    } catch (error: any) {
       console.log(error);
+      if (error.code === "auth/invalid-credential") {
+        setErrorMessage("Email or Password is Incorrect.");
+      } else if (error.code === "auth/wrong-password") {
+        setErrorMessage("Incorrect password. Please try again.");
+      } else {
+        setErrorMessage("Error Login user: " + error.message);
+      }
     }
   }
 
   if (userLoggedIn) {
     return <Navigate to={"/"} replace={true} />;
   }
-  return <LoginRegistrationForm isLogin={true} handleSubmit={handleSubmit} />;
+  return (
+    <LoginRegistrationForm
+      isLogin={true}
+      handleSubmit={handleSubmit}
+      errorMessage={errorMessage}
+    />
+  );
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,9 +2,11 @@ import { Navigate } from "react-router-dom";
 import LoginRegistrationForm from "../components/LoginRegForm";
 import { useAuth } from "../context/authContext";
 import { registerUserWithEmailAndPassword } from "../firebase/auth";
+import { useState } from "react";
 
 export default function Register() {
   const { userLoggedIn } = useAuth();
+  const [errorMessage, setErrorMessage] = useState("");
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -12,16 +14,40 @@ export default function Register() {
       const formData = new FormData(e.currentTarget);
       const email = await formData.get("userEmail")?.toString().toLowerCase();
       const password = await formData.get("userPassword")?.toString();
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (!email?.match(emailRegex)) {
+        setErrorMessage("Please enter a valid email address.");
+        return;
+      }
+
+      if (password?.length! < 6) {
+        setErrorMessage("Password must be at least 6 characters long.");
+        return;
+      }
 
       await registerUserWithEmailAndPassword(email!, password!);
-    } catch (error) {
-      alert(`Error registering user: ${error}`);
+      setErrorMessage("User Registered Successfully!");
+    } catch (error: any) {
       console.log(error);
+      if (error.code === "auth/email-already-in-use") {
+        setErrorMessage(
+          "This email is already in use. Please use a different email."
+        );
+      } else {
+        setErrorMessage("Error registering user: " + error.message);
+      }
     }
   }
 
   if (userLoggedIn) {
     return <Navigate to={"/"} replace={true} />;
   }
-  return <LoginRegistrationForm isLogin={false} handleSubmit={handleSubmit} />;
+  return (
+    <LoginRegistrationForm
+      isLogin={false}
+      handleSubmit={handleSubmit}
+      errorMessage={errorMessage}
+    />
+  );
 }
